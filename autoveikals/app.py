290c0e5b828afac29df84335b71d2d19e2d66ad7
samuelsmarkevichs-source <samuels.markevichs@gmail.com>
2026-05-06 22:02:56 +0300

    did some changes

--- autoveikals/app.py
+++ autoveikals/app.py
@@ -39,17 +39,11 @@ def about():
     return render_template("about.html")
 
 def get_db_connection():
-    db = Path(__file__).parent / "miniveikals"
+    db = Path(__file__).parent / "miniveikals.db"
     conn = sqlite3.connect(db)
     conn.row_factory = sqlite3.Row
     return conn
 
-# products = [
-# {"id": 1, "name": "Zīmulis", "price": 0.50, "image": "pencil.jpg"},
-# {"id": 2, "name": "Pildspalva", "price": 1.20, "image": "pen.jpg"},
-# {"id": 3, "name": "Burtnīca", "price": 0.90, "image": "notebook.jpg"},
-# {"id": 4, "name": "Mape", "price": 1.50, "image": "folder.jpg"},
-# ]
 
 if __name__ == "__main__":
     app.run(debug=True)
